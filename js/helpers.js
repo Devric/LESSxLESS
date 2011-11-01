89ebd8966bec790afa65d45b0d4c6e708a2bdbd5
Devric <billbox13@gmail.com>
2011-11-01 13:22:11 +1100

    [add] image size controller for gallery

--- js/helpers.js
+++ js/helpers.js
@@ -1,14 +1,45 @@
-// index
-// - smooth scroll function goToByScroll(id);
+// | index
+// | -----------------
+// | - document function
+// | -----------------
+
+// | -----------------
+// | - window function
+// | -----------------
+// | -- image size controller
+
+// | -----------------
+// | - helper function
+// | -----------------
+// | -- smooth scroll function goToByScroll(id);
+
+
+// -----------------
+// document ready
+// -----------------
 $(document).ready(function() {
 
+});
+
+
+// -----------------
+// window ready
+// -----------------
+$(window).load(function(){ 
+    // img size controller
+    $('.item').each(function(){
+        var obj = $(this),
+            maxside = obj.width(),
+            h = obj.find('img').height();
+        if(h > maxside){
+            obj.find('img').css({height:maxside / 1.1, width:'auto', 'margin-left': '20%'});
+        }
+    });
 });
 
 // -----------------
 // helpers functions
 // -----------------
-
-
 
 // smooth scroll to id jquery plugin  $('#fsetbtn').removeAttr("href").click(function(){ goToByScroll('fireset'); });
 function goToByScroll(id){
